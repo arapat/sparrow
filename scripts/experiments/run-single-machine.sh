ce91b320492f5bc63bfc26a424d35ddf8c9698f0
Julaiti Alafate <jalafate@gmail.com>
2018-04-29 16:06:07 +0000

    new script for experiments on small feature sizes

--- scripts/experiments/run-single-machine.sh
+++ scripts/experiments/run-single-machine.sh
@@ -7,13 +7,24 @@ echo
 cat config.json
 echo
 
-for i in `seq 1 35`; do
+for i in `seq 8 35`; do
     head=$((280 - i * 8))
     tail=$((284 + i * 8))
     echo "Testing on range [$head, $tail]"
     RUST_LOG=DEBUG cargo run --release single $head $tail 500 2> run-$i.log
-    mkdir run$i
-    mv model-*.json *.log run$i
+    mkdir run-large-$i
+    mv model-*.json *.log run-large-$i
+    rm *.bin
+done
+echo
+
+for i in `seq 1 28`; do
+    head=$((282 - i * 2))
+    tail=$((282 + i * 2))
+    echo "Testing on range [$head, $tail]"
+    RUST_LOG=DEBUG cargo run --release single $head $tail 500 2> run-$i.log
+    mkdir run-large-$i
+    mv model-*.json *.log run-large-$i
     rm *.bin
 done
 echo
