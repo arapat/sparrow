3ca8065cec3443ee976ef1a264d0301551b9c1b4
Julaiti Alafate <jalafate@gmail.com>
2018-04-30 00:14:11 +0000

    repeat experiments

--- scripts/experiments/run-single-machine.sh
+++ scripts/experiments/run-single-machine.sh
@@ -9,25 +9,31 @@ echo
 cat config.json
 echo
 
+for j in `seq 1 10`; do
 for i in `seq 8 35`; do
     head=$((280 - i * 8))
     tail=$((284 + i * 8))
     echo "Testing on range [$head, $tail]"
     RUST_LOG=DEBUG timeout $TIMEOUT cargo run --release single $head $tail 100 2> run-$i.log
     mkdir timeout-large-$i
-    mv model-*.json *.log timeout-large-$i
-    rm *.bin
+    mv model-*.json timeout-large-$i/
+    mv run-$i.log timeout-large-$i/run-$i-$j.log
+    rm *.log *.bin
+done
 done
 echo
 
+for j in `seq 1 10`; do
 for i in `seq 1 28`; do
     head=$((282 - i * 2))
     tail=$((282 + i * 2))
     echo "Testing on range [$head, $tail]"
     RUST_LOG=DEBUG timeout $TIMEOUT cargo run --release single $head $tail 100 2> run-$i.log
     mkdir timeout-small-$i
-    mv model-*.json *.log timeout-small-$i
-    rm *.bin
+    mv model-*.json timeout-small-$i/
+    mv run-$i.log timeout-small-$i/run-$i-$j.log
+    rm *.log *.bin
+done
 done
 echo
 
