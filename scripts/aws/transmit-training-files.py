4b73d9dd51c7269a06bbb7d782fc920159249545
Julaiti Alafate <jalafate@gmail.com>
2018-05-01 22:48:48 +0000

    fixed some issues

--- scripts/aws/transmit-training-files.py
+++ scripts/aws/transmit-training-files.py
@@ -15,15 +15,31 @@ with open(neighbor_path) as f:
     remain = f.readlines()
 remain = [line.strip() for line in remain]
 remain = [line for line in remain if line]
+print("Read %d neighbors." % len(remain))
 
 cur_remote = None
 next_remote = None
 proc_send_file = None
 
+for addr in remain:
+    command = "ssh -o StrictHostKeyChecking=no -i {} {} rm {}" \
+                    .format(ident_file, addr, neighbor_path)
+    proc_init = subprocess.Popen(command.split())
+    proc_init.wait()
+with open(neighbor_path, 'w') as f:
+    f.write('\n'.join(remain))
+
 while childs or remain:
     if proc_send_file is None and remain:
         next_remote = remain[0]
         remain = remain[1:]
+        command = "ssh -o StrictHostKeyChecking=no -i {} {} test -f {}" \
+                        .format(ident_file, next_remote, neighbor_path)
+        proc_check = subprocess.Popen(command.split())
+        proc_check.wait()
+        if proc_check.returncode == 0:
+            print("{} has already been working.".format(next_remote))
+            continue
         command = "ssh -o StrictHostKeyChecking=no -i {} {} test -f /mnt/training.bin" \
                         .format(ident_file, next_remote)
         proc_check_file = subprocess.Popen(command.split())
@@ -43,17 +59,20 @@ while childs or remain:
         proc_send_file = None
 
     if cur_remote is not None and len(remain) >= 2:
+        print("launching new worker at {}.".format(cur_remote))
         mid = int(len(remain) / 2)
-        with open("/tmp/load.txt", 'w') as f:
+        filepath = "/tmp/load_{}.txt".format(cur_remote)
+        with open(filepath, 'w') as f:
             f.write('\n'.join(remain[:mid]))
-        command = "scp -o StrictHostKeyChecking=no -i {} /tmp/load.txt ubuntu@{}:{}" \
-                        .format(ident_file, cur_remote, neighbor_path)
+        command = "scp -o StrictHostKeyChecking=no -i {} {} ubuntu@{}:{}" \
+                        .format(ident_file, filepath, cur_remote, neighbor_path)
         proc_send_neighbor = subprocess.Popen(command.split())
         proc_send_neighbor.wait()
-        command = "python3 {}".format(this_file)
+        command = "ssh -o StrictHostKeyChecking=no -i {} {} python3 {}" \
+                        .format(ident_file, cur_remote, this_file)
         proc_helper = subprocess.Popen(command.split())
         childs.append((cur_remote, proc_helper))
-        print("Launched a new helper at {}.".format(cur_remote))
+        print("Launched a new helper at {} with {} neighbors.".format(cur_remote, mid))
 
         cur_remote = None
         remain = remain[mid:]
