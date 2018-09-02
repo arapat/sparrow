ea60eff62fba9e67d9430bb1f7e87f16d3f2d699
Julaiti Alafate <jalafate@gmail.com>
2018-09-02 11:40:03 -0700

    created examples

---
+++ examples/splice.rs
@@ -0,0 +1,28 @@
+extern crate log;
+extern crate env_logger;
+extern crate time;
+
+extern crate rustboost;
+
+use std::io::Write;
+use time::get_time;
+
+use rustboost::run_rust_boost;
+
+
+fn main() {
+    let curr_time = get_time().sec;
+    env_logger::Builder::from_default_env()
+        .format(move |buf, record| {
+            let timestamp = get_time();
+            let formatted_ts = format!("{}.{}", timestamp.sec - curr_time, timestamp.nsec);
+            writeln!(
+                buf, "{}, {}, {}, {}",
+                record.level(), formatted_ts, record.module_path().unwrap(), record.args()
+            )
+        })
+        .init();
+
+    let config_file = String::from("../rust-boost/examples/config2.yaml");
+    run_rust_boost(config_file);
+}
