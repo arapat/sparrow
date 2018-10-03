e4b92807d794385978152ef098a05acaac27977f
Julaiti Alafate <jalafate@gmail.com>
2018-10-02 17:13:16 -0700

    clean up bins

--- src/boosting/bins.rs
+++ src/boosting/bins.rs
@@ -14,19 +14,19 @@ pub struct Bins {
     vals: Vec<f32>
 }
 
-struct BinMapper {
+struct DistinctValues {
     total_vals: usize,
     distinct: BTreeMap<NotNaN<f32>, u32>
 }
 
 impl Bins {
-    fn new(size: usize, mapper: &BinMapper) -> Bins {
-        let avg_bin_size = (mapper.total_vals / size) as usize;
+    fn new(size: usize, distinct_vals: &DistinctValues) -> Bins {
+        let avg_bin_size = (distinct_vals.total_vals / size) as usize;
         let mut last_val = 0.0;
         let mut counter = 0;
         let mut vals: Vec<f32> = vec![];
-        mapper.distinct.iter().for_each(|(wrapped_k, v)| {
-            let k = wrapped_k.into_inner();
+        distinct_vals.distinct.iter().for_each(|(k, v)| {
+            let k = k.into_inner();
             if counter > avg_bin_size {
                 vals.push((last_val + k) / 2.0);
                 counter = 0;
@@ -34,9 +34,8 @@ impl Bins {
             counter += *v as usize;
             last_val = k;
         });
-        let actual_size = vals.len();
         Bins {
-            size: actual_size,
+            size: vals.len(),
             vals: vals
         }
     }
@@ -52,61 +51,67 @@ impl Bins {
     }
 }
 
-impl BinMapper {
-    fn new() -> BinMapper {
-        BinMapper {
+impl DistinctValues {
+    fn new() -> DistinctValues {
+        DistinctValues {
             total_vals: 0,
             distinct: BTreeMap::new()
         }
     }
 
-    fn update(&mut self, val: &f32) {
+    fn update(&mut self, val: f32) {
         self.total_vals += 1;
-        let count = self.distinct.entry(NotNaN::new(*val).unwrap()).or_insert(0);
+        let count = self.distinct.entry(NotNaN::new(val).unwrap()).or_insert(0);
         *count += 1;
     }
 }
 
 
-/// Find the percentiles of the feature in the range specified by `range`. 
+/// Create bins for the features in the range specified by `range`
 ///
-/// To decide the percentiles, the function reads `max_sample_size` number 
-/// of examples from `data_loader`.
-/// The number of selected percentiles is `max_bin_size`.
-///
-/// If the number of different values on this feature dimension `k` is smaller
-/// than `max_bin_size`, only `k` percentiles would return.
-pub fn create_bins(max_sample_size: usize, max_bin_size: usize,
-                   range: &Range<usize>, data_loader: &mut BufferLoader) -> Vec<Bins> {
+/// * `max_sample_size`: The number of examples to read for deciding the splitting
+///   thresholds for creating bins
+/// * `max_bin_size`: The total number of bins created for each freature. The actual
+///   number of bins might be smaller if there are fewer distinct values for a feature
+/// * `range`: The range of the features this worker is responsible for
+/// * `data_loader`: Data loader for providing training examples
+pub fn create_bins(
+    max_sample_size: usize,
+    max_bin_size: usize,
+    range: &Range<usize>,
+    data_loader: &mut BufferLoader,
+) -> Vec<Bins> {
     let start = range.start;
     let range_size = range.end - start;
-    let mut mappers: Vec<BinMapper> = Vec::with_capacity(range_size);
+    let mut distinct: Vec<DistinctValues> = Vec::with_capacity(range_size);
     let mut remaining_reads = max_sample_size;
 
     for _ in 0..range_size {
-        mappers.push(BinMapper::new());
+        distinct.push(DistinctValues::new());
     }
     while remaining_reads > 0 {
-        let data = data_loader.get_next_batch(false);
+        let data = data_loader.get_next_batch(false /* no switch buffers */);
         data.iter().for_each(|example| {
             let features = example.0.get_features();
-            mappers.iter_mut()
-                   .enumerate()
-                   .for_each(|(idx, mapper)| {
-                       mapper.update(&(features[idx + start] as f32));
-                   });
+            distinct.iter_mut()
+                    .enumerate()
+                    .for_each(|(idx, mapper)| {
+                        mapper.update(features[start + idx] as f32);
+                    });
         });
         remaining_reads -= data.len();
     }
-    let ret: Vec<Bins> = mappers.iter()
-                                .map(|mapper| Bins::new(max_bin_size, mapper))
-                                .collect();
+    let ret: Vec<Bins> = distinct.iter()
+                                 .map(|mapper| Bins::new(max_bin_size, mapper))
+                                 .collect();
 
+    // Logging
     info!("Bins are created.");
     ret.iter()
        .enumerate()
        .for_each(|(idx, bin)| {
-           info!("{} thresholds are generated for the feature {}.", bin.len(), idx + start);
+           info!("{} thresholds are generated for the feature {}.", bin.len(), start + idx);
        });
+
     ret
 }
